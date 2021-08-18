30b345aa1544abf0c9e971310307def8ef582fdf
Daniel <spyker156@gmail.com>
2021-08-18 11:49:36 +0300

    bug fix

--- src/Models/MF.Models.ViewModels/Topics/TopicCreateInputModel.cs
+++ src/Models/MF.Models.ViewModels/Topics/TopicCreateInputModel.cs
@@ -6,17 +6,17 @@
 
     public class TopicCreateInputModel
     {
-        [Required(ErrorMessage = "NAME E ZADYLJITELEN IDIOT")]
-        [MinLength(NameMinLength, ErrorMessage = "NEMOJE PO MALKO OT 2 SIMVOLA")]
-        [MaxLength(NameMaxLength, ErrorMessage = "MAX LENGHT 300 KONQ")]
+        [Required]
+        [MinLength(2)]
+        [MaxLength(100)]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "NEMOJE BEZ KATEGORIQ ID")]
+        [Required]
         public int? CategoryId { get; set; }
 
-        [Required(ErrorMessage = "NEMOJE BEZ CONTENT!!")]
-        [MinLength(ContentMinLength, ErrorMessage = "NEMOJE BEZ po-malko simvoli!!")]
-        [MaxLength(ContentMaxLength, ErrorMessage = "NEMOJE mnogo simvoli!")]
+        [Required]
+        [MinLength(2)]
+        [MaxLength(100)]
         public string Content { get; set; }
     }
 }
